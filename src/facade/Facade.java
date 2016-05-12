2751ca62cb94167728137aa52fe829c6b49bdc73
Ranyelson Dantas <nielsonnp@gmail.com>
2016-05-12 11:12:28 -0300

    Atualização classe Facade e inclusão do Control

--- src/facade/Facade.java
+++ src/facade/Facade.java
@@ -4,8 +4,24 @@ import java.util.ArrayList;
 
 public class Facade {
 	
-	public static boolean cadastrarUsuario {
-		
-	}
+	 public static boolean cadastrarUsuario(Usuario usuario){
+	        return ;
+	    }
 
+	 
+	 public static boolean editarUsuario(Usuario usuario){
+	        return ;
+	    }
+	 
+	 
+	 public static boolean deletarUsuario(Usuario usuario){
+	        return ;
+	    }
+	 
+	 	 
+	 public static ArrayList<Usuario> listarUsuarios(){
+	        return ;
+	    }
+	 
+	 
 }
