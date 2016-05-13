dd89fdce246328909006d7540f3c2a4aa038f1cf
Ranyelson Dantas <nielsonnp@gmail.com>
2016-05-13 09:47:45 -0300

    Implemento classe Facade

--- src/facade/Facade.java
+++ src/facade/Facade.java
@@ -2,26 +2,26 @@ package facade;
 
 import java.util.ArrayList;
 
+
 public class Facade {
 	
 	 public static boolean cadastrarUsuario(Usuario usuario){
-	        return ;
+	        return UsuarioControl.cadastrarUsuario(usuario);
 	    }
 
 	 
 	 public static boolean editarUsuario(Usuario usuario){
-	        return ;
+	        return UsuarioControl.editarUsuario(usuario);
 	    }
 	 
 	 
 	 public static boolean deletarUsuario(Usuario usuario){
-	        return ;
+	        return UsuarioControl.deletarUsuario(usuario;
 	    }
 	 
 	 	 
 	 public static ArrayList<Usuario> listarUsuarios(){
-	        return ;
+	        return UsuarioControl.listarUsuario(;
 	    }
-	 
-	 
+	 	 
 }
